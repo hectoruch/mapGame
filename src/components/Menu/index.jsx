8c2ff75b0becdf00b24cf20498409630c594cb3a
Hector Arce de las Heras <hector@MacBook-Pro-de-Hector.local>
2016-12-21 00:51:44 +0100

    add logo and improve styles for menu component

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,13 +1,19 @@
+import whiteLogo from './assets/logoWhite.png';
+
 import React from 'react';
 import $ from 'jquery';
 import './style.scss';
 import { Router, Route, Link, browserHistory } from 'react-router';
+
 
 class Menu extends React.Component{
 
   render() {
     return (
       <div className="c-menu">
+        <Link className="-no-underline" to="/">
+          <img src={whiteLogo} alt="logo" />
+        </Link>
         <nav>
           <div className="contain-buttons">
             <Link to="login" className="butn -submit">Start Game</Link>
